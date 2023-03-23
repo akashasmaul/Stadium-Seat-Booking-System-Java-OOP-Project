d6c6b1c8ef23009d5217062469bfbc7558fa91ff
akashasmaul <akashasmaulhossain@gmail.com>
2023-03-23 20:46:10 +0600

    Console Application

---
+++ src/project/FinalTerm/Main.java
@@ -0,0 +1,35 @@
+package project.FinalTerm;
+import java.util.Scanner;
+public class Main {
+	Scanner input = new Scanner(System.in);
+	void  main () {
+		Final ob = new Final();
+		ob.login();
+		
+	ob.showOption();
+	ob.selectOption();
+	ob.Payment();
+	ob.reciept();
+	
+	int i = 0;
+	while  (i < 1000) {
+	
+	System.out.println("\t\nPress any key to go back to Main Menu");
+	System.out.print("\tSelect option: ");
+	String op = input.nextLine();
+	 if (op == "1") 
+	{
+		 	ob.showOption();
+			ob.selectOption();
+			ob.Payment();
+			ob.reciept();
+	}
+	else {
+		ob.showOption();
+		ob.selectOption();
+		ob.Payment();
+		ob.reciept();
+}
+}
+}
+}
---
+++ src/project/FinalTerm/Test.java
@@ -0,0 +1,11 @@
+package project.FinalTerm;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Main m = new Main ();
+		m.main();
+	}
+
+}
